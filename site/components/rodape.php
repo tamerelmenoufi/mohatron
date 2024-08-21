9ec74219771393f2d0237d48c301212281a9cffe
livia128924 <livia128924@gmail.com>
2024-08-21 18:13:22 -0300

    Atualizações

--- site/components/rodape.php
+++ site/components/rodape.php
@@ -20,52 +20,11 @@ background:none;
   <!-- ======= Footer ======= -->
   
 
-<footer id="footer" class="footer" style="background:#144397">
-<div class="container text-center footer-legal" >
-  <div class="row align-items-center">
-    <div class="col-md-3  ">
-    <p style="color:#fff;font-size:18px;text-align:center;font-weight:bold;font-style:italic;margin-bottom:0px">UNIDADE CIDADE NOVA</p>
-<p style="color:#fff">Rua Prof. felix Valois, 61<br> Cidade nova - Manaus/AM</p>
-    </div>
-    <div class="col-md-3">
-    <p style="color:#fff;font-size:18px;text-align:center;font-weight:bold;font-style:italic;margin-bottom:0px">UNIDADE MANOA</p>
-<p style="color:#fff">Av. Francisco Queiroz, 02  <br> Manoa - Manaus/AM</p>
+<footer id="footer" class="footer" style="background:#0f77ac">
 
-    </div>
-    <div class="col-md-6">
-      <div class="social-links">
-    <?php
-
-$query = "select * from configuracoes where codigo = '1'";
-$result = sisLog( $query);
-$d = mysqli_fetch_object($result);
-
-$midias = json_decode($d->midias_sociais);
-
-$midias_sociais = [
-  'facebook' => 'https://www.facebook.com/',
-  'twitter' => 'https://twitter.com/',
-  'instagram' => 'https://instagram.com/',
-  'youtube' => 'https://www.youtube.com/',
-  'linkedin' => 'https://www.linkedin.com/',
-  'whatsapp' => 'https://api.whatsapp.com/send?phone='
-];
-
-foreach($midias_sociais as $ind => $url){
-  if($midias->$ind){
-?>
-<a href="<?=$url.$midias->$ind?>" target="_black" class="<?=$ind?>"><i class="bi bi-<?=$ind?>"></i></a>
-<?php
-  }
-}
-?> 
-</div>
-    </div>
-  </div>
-</div>
 
 <div style="text-align:center;font-size:14px;padding-bottom:2px;margin-top:40px"><div class="row g-0"><div class="col-md-6 text-center text-md-end">
-  &copy; Copyright <strong><span>capitalsolucoes</span></strong>.
+  &copy; Copyright <strong><span>Mohatron Soluções em TI</span></strong>.
 </div><div class="col-md-6 text-center text-md-start">
   <a class="janela" janela="components/popup.php" style="color:#fff; text-decoration:underline; cursor:pointer" >Todos os direitos reservados </a>
 </div></div> 
