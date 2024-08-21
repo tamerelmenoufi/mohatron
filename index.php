4b901c10fbad5a60b5be3795d6ec985bc5b0af53
tamerelmenoufi <tamer@mohatron.com.br>
2024-08-21 12:21:35 -0400

    Atualizações

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <center>
 <br><br><br><br><br>
-<img src="img/logo.jpeg" alt="">
+<img src="img/logo.png" alt="">
 <h1 style="font-family:verdana">SITE EM DESENVOLVIMENTO</h1>
 <h1 style="font-family:verdana">AJUSTES NO SISTEMA</h1>
 </center>
