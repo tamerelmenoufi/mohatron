f34e30b019f4475ca067622805e24399671a72c8
livia128924 <livia128924@gmail.com>
2024-08-22 18:16:14 -0300

    Atualizações

--- site/components/sms.php
+++ site/components/sms.php
@@ -1,4 +1,15 @@
-<div>
 
-teste
+  
+  <div class=" ">
+    <div class="col-6">
+    <img src="assets/img/img-sms.png" class="img-responsive" style="">
+    </div>
+
+    
+    <div class="col-6">
+      One of two columns
+    </div>
+  
+  
+
 </div>
