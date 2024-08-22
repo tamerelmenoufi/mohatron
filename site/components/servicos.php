d92903814f5909134171d2fc4089f24b9f374d83
livia128924 <livia128924@gmail.com>
2024-08-22 13:52:43 -0300

    Atualizações

--- site/components/servicos.php
+++ site/components/servicos.php
@@ -111,68 +111,21 @@
         </div>
 
         <div class="row">
-
-
-          <?php
-          $query = "select * from servicos where situacao = '1' order by codigo desc limit 0,3";
-          $result = sisLog( $query);
-          while($d = mysqli_fetch_object($result)){
-          ?>
-
-          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
-            <div class="post-box"  style="background:#fff;padding:13px">
-              <div style="margin-top:10px;height:267px" class="post-img"><img src="<?=$localPainel?>site/volume/servicos/<?=$d->imagem?>" class="img-fluid" alt=""></div>
-              <!-- <div class="meta">
-                <span class="post-date">Tue, December 12</span>
-                <span class="post-author"> / Julia Parker</span>
-              </div> -->
-              <a href="servico.php?cod=<?=$d->codigo?>"><h3 class="post-title" style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;color:#144397"><?=$d->titulo?></h3>
-              <p style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;color:#144397"><?=strip_tags(str_replace('<',' <',str_replace('>','> ',$d->materia)))?></p> </a>
-             <!-- <a href="servico.php?cod=<?=$d->codigo?>" class="mt-3 mb-3">
-                <button type="button" class="botao botaoroxo">Leia Mais<i class="bi bi-arrow-right"></i></button>
-              </a>-->
-            </div>
-          </div>
-
-          <?php
-          }
-          ?>
-
-          <!-- <div class="col-lg-4" data-aos="fade-up" data-aos-delay="400">
-            <div class="post-box">
-              <div class="post-img"><img src="assets/img/blog/blog-2.jpg" class="img-fluid" alt=""></div>
-              <div class="meta">
-                <span class="post-date">Fri, September 05</span>
-                <span class="post-author"> / Mario Douglas</span>
-              </div>
-              <h3 class="post-title">Et repellendus molestiae qui est sed omnis voluptates magnam</h3>
-              <p>Voluptatem nesciunt omnis libero autem tempora enim ut ipsam id. Odit quia ab eum assumenda. Quisquam omnis aliquid necessitatibus tempora consectetur doloribus...</p>
-              <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div>
-
-          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="600">
-            <div class="post-box">
-              <div class="post-img"><img src="assets/img/blog/blog-3.jpg" class="img-fluid" alt=""></div>
-              <div class="meta">
-                <span class="post-date">Tue, July 27</span>
-                <span class="post-author"> / Lisa Hunter</span>
-              </div>
-              <h3 class="post-title">Quia assumenda est et veritatis aut quae</h3>
-              <p>Quia nam eaque omnis explicabo similique eum quaerat similique laboriosam. Quis omnis repellat sed quae consectetur magnam veritatis dicta nihil...</p>
-              <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
-            </div>
-          </div> -->
-
-
-         <!-- <center style="margin-top:20px">
-          <a href="servico_categoria.php">
-         <button type="button" class=" botaobranco">
-          Outros serviços
-        </button></a>
-      </center>-->
-
-      <div style="padding:10px"> </div>
+        <li class="wow fadeIn" data-wow-duration="1.2s" style="visibility: visible; animation-duration: 1.2s; animation-name: fadeIn;">
+									<div class="inner_list">
+										<div class="service_icon">
+											<svg xmlns="http://www.w3.org/2000/svg" id="icons" enable-background="new 0 0 64 64" height="512" viewBox="0 0 64 64" width="512" class="svg replaced-svg"><path d="m29.03 5c.552 0 1-.447 1-1s-.448-1-1-1h-.01c-.552 0-.994.447-.994 1s.452 1 1.004 1z"></path><path d="m32 5h3c.553 0 1-.447 1-1s-.447-1-1-1h-3c-.553 0-1 .447-1 1s.447 1 1 1z"></path><path d="m50 57v-50.998c0-3.305-2.685-5.992-5.987-5.999-.004 0-.008-.003-.013-.003h-24c-3.309 0-6 2.691-6 6v51c0 3.859 3.141 7 7 7h22c3.859 0 7-3.141 7-7zm-2-7h-32v-41.998h32zm-32 2h16 16v1c0 2.757-2.243 5-5 5h-11-11c-2.757 0-5-2.243-5-5zm4-50h23.99c.004 0 .006.002.01.002 2.206 0 4 1.794 4 4h-32v-.002c0-2.206 1.794-4 4-4zm23 60h-22c-2.418 0-4.438-1.726-4.9-4.01 1.264 1.241 2.994 2.01 4.9 2.01h22c1.906 0 3.636-.769 4.9-2.01-.462 2.284-2.482 4.01-4.9 4.01z"></path><path d="m29 55c0 1.654 1.346 3 3 3s3-1.346 3-3-1.346-3-3-3-3 1.346-3 3zm3-1c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z"></path><path d="m23 42.998h18c2.757 0 5-2.243 5-5v-18c0-2.757-2.243-5-5-5h-18c-2.757 0-5 2.243-5 5v18c0 2.757 2.243 5 5 5zm-3-23c0-1.654 1.346-3 3-3h18c1.654 0 3 1.346 3 3v18c0 1.654-1.346 3-3 3h-18c-1.654 0-3-1.346-3-3z"></path><path d="m28.001 32.002c.256 0 .512-.098.707-.293.391-.391.391-1.023 0-1.414l-1.292-1.292 1.292-1.293c.391-.391.391-1.023 0-1.414s-1.023-.391-1.414 0l-1.999 2c-.391.391-.391 1.023 0 1.414l1.999 1.999c.195.195.451.293.707.293z"></path><path d="m35.299 31.709c.195.195.451.293.707.293s.512-.098.707-.293l1.999-1.999c.391-.391.391-1.023 0-1.414l-1.999-2c-.391-.391-1.023-.391-1.414 0s-.391 1.023 0 1.414l1.292 1.293-1.292 1.292c-.391.391-.391 1.023 0 1.414z"></path><path d="m30.687 32.951c.105.035.212.052.316.052.419 0 .81-.265.949-.684l1.998-6c.175-.524-.108-1.091-.632-1.265-.527-.175-1.091.108-1.265.632l-1.999 6c-.174.525.109 1.091.633 1.265z"></path><path d="m32 40c6.065 0 11-4.935 11-11s-4.935-11-11-11-11 4.935-11 11 4.935 11 11 11zm0-20c4.963 0 9 4.037 9 9s-4.037 9-9 9-9-4.037-9-9 4.037-9 9-9z"></path></svg>
+										</div>
+										<div class="service_title">
+											<h3>Desenvolvedor de Aplicativos</h3>
+										</div>
+										<div class="service_definition">
+											<p>Desenvolvimento de Aplicativos para Celulares Android e iOS (iPhone).</p>
+										</div>
+										<span class="first"></span>
+										<span class="second"></span>
+									</div>
+								</li>
         </div>
 
       
