41db7320b1c562c8870413e40cd4fc71249cd46a
livia128924 <livia128924@gmail.com>
2024-08-22 10:15:27 -0300

    Atualizações

--- site/components/apresentacao.php
+++ site/components/apresentacao.php
@@ -1,4 +1,40 @@
-<div class="container">
+<style>
+.bt-azul{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #144397;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+}
+
+.bt-azul:hover{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #32589b;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+    -webkit-animation: pulse 500ms;
+    animation: pulse 500ms;
+    background-color: #333 !important;
+}
+
+</style>
+
+<div class="container" style="margin-top:120px">
 <div class="row">
     <div class="col-sm-5 col-md-6">
 
@@ -13,17 +49,17 @@
          Médio e Grande Porte.</p>
 
     <p class="o-7" style="text-align: justify;">
-    No mercado desde 2009, o Programador Digital desenvolve ferramentas digitais modernas,
+    No mercado desde 2009, a Mohatron Soluções em TI desenvolve ferramentas digitais modernas,
      dinâmicas e de alta tecnologia, sempre com foco em resultados.
       A experiência e expertise dos nossos profissionais é o grande diferencial nos 
       trabalhos que executamos, pois em um mercado competitivo, para a sua empresa 
       fazer bons negócios, é necessário que a estrutura online seja profissional.</p>
 
     <p>
-        <a class="btn btn-primary btn-lg m-t-2" href="#">Fale com especialista?</a>
+        <a class=" btn-lg m-t-2 bt-azul" href="#">Fale com especialista?</a>
     </p>
 
-    
+
     </div>
         </div>
             </div>
