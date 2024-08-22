185e0c1ee0ee2e2bd2d12a1cc62cc906c5124277
livia128924 <livia128924@gmail.com>
2024-08-22 18:26:10 -0300

    Atualizações

--- site/components/sms.php
+++ site/components/sms.php
@@ -1,14 +1,14 @@
 <div class="container">
   
   <div class="row ">
-    <div class="col-6">
+    <div class="col">
     <div>
       <img src="assets/img/img-sms.png" class="img-fluid im-responsive" style="">
 </div>
     </div>
 
     
-    <div class="col-6">
+    <div class="col">
       One of two columns
     </div>
   
