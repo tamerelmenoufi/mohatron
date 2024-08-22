0fd05df1d93f84673077085b54a31d6d210a5097
livia128924 <livia128924@gmail.com>
2024-08-22 10:24:39 -0300

    Atualizações

--- site/components/apresentacao.php
+++ site/components/apresentacao.php
@@ -32,6 +32,83 @@
 
 }
 
+HTML SCSSResult Skip Results Iframe
+EDIT ON
+$color: #0cf;
+
+.button {
+  display: inline-block;
+  padding: .75rem 1.25rem;
+  border-radius: 10rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 1rem;
+  letter-spacing: .15rem;
+  transition: all .3s;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: $color;
+    border-radius: 10rem;
+    z-index: -2;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0%;
+    height: 100%;
+    background-color: darken($color, 15%);
+    transition: all .3s;
+    border-radius: 10rem;
+    z-index: -1;
+  }
+  &:hover {
+    color: #fff;
+    &:before {
+      width: 100%;
+    }
+  }
+}
+
+/* optional reset for presentation */
+* {
+  font-family: Arial;
+  text-decoration: none;
+  font-size: 20px;
+}
+.container {
+  padding-top: 50px;
+  margin: 0 auto;
+  width: 100%;
+  text-align: center;
+}
+h1 {
+  text-transform: uppercase;
+  font-size: .8rem;
+  margin-bottom: 2rem;
+  color: #777;
+}
+span {
+  display: block;
+  margin-top: 2rem;
+  font-size: .7rem;
+  color: #777;
+  a {
+    font-size: .7rem;
+    color: #999;
+    text-decoration: underline;
+  }
+}
+
 </style>
 
 <div class="container" style="margin-top:120px">
@@ -59,6 +136,15 @@
         <a class=" btn-lg m-t-2 bt-azul" href="#">Fale com especialista?</a>
     </p>
 
+
+    <div class="container">
+  <h1>Pure Css Button Hover effect</h1>
+  
+  <a href="#" class="button">Hover me</a>
+  
+<!--  optional  -->
+  <span>Made by <a href="http://alticreation.com/en">alticreation.com</a></span>
+</div> 
 
     </div>
         </div>
