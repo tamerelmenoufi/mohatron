cf09814b664bc1a8e7c61cab84403415783cb5ea
livia128924 <livia128924@gmail.com>
2024-08-22 20:46:47 -0300

    Atualizações

--- site/components/sms.php
+++ site/components/sms.php
@@ -9,7 +9,8 @@
 
     
     <div class="col-md-6">
-      One of two columns
+      
+    <p style="font-size:30px;color:#fff;font-weight:bold">Crie experiências de SMS que permitam que você alcance os usuários finais como e quando quiser</p>
     </div>
   
   
