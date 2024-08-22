810d4f493c6c558110ddee91ba60158d83d69c4a
livia128924 <livia128924@gmail.com>
2024-08-22 17:05:45 -0300

    Atualizações

---
+++ site/components/sms.php
@@ -0,0 +1,17 @@
+<div class="container">
+  
+  <div class="row ">
+    <div class="col-6">
+      One of two columns
+    </div>
+
+    
+    <div class="col-6">
+      One of two columns
+    </div>
+  
+  
+  
+  
+  </div>
+  </div>
