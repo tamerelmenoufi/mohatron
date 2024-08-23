0649c2ffeb182afbf63de059e6ab11bd862c6b9f
livia128924 <livia128924@gmail.com>
2024-08-22 21:02:18 -0300

    Atualizações

--- site/components/sms.php
+++ site/components/sms.php
@@ -11,38 +11,22 @@
     <div class="col-md-6">
 
        
-    <p style="font-size:30px;color:#fff;font-weight:bold">
+    <p style="font-size:30px;color:#005992;font-weight:bold">
       Crie experiências de SMS que permitam que você alcance os usuários finais como e quando quiser
     </p>
 
 
-    <div class="row">
+    <div class="row g-0">
        <div class="col-md-6">
-<li>
-  <span>
-    <p>
-      <font style="vertical-align: inherit;">
-        <font style="vertical-align: inherit;">
-          Envie e receba mensagens de texto de clientes do mundo todo de forma programática.
-        </font>
-      </font>
+    <p style="font-size:15px;border-left: 2px solid #005992 ">
+          Envie e receba mensagens de texto de clientes do mundo todo de forma programática.     
     </p>
-  </span>
-</li>
-
-<li>
-  <span>
+    
     <p>
-      <font style="vertical-align: inherit;">
-        <font style="vertical-align: inherit;">
           Reduza as preocupações com conformidade — o mecanismo de conformidade 
           global da Vonage lida com as complexidades da comunicação mundial e adere 
           aos padrões de operadoras e redes em todo o mundo.
-        </font>
-      </font>
     </p>
-  </span>
-</li>
 </div>
 
        <div class="col-md-6">
