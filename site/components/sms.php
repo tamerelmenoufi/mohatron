ff8434b400e4c2ede759a6aeabfaba1ec71a48f2
livia128924 <livia128924@gmail.com>
2024-08-22 21:09:27 -0300

    Atualizações

--- site/components/sms.php
+++ site/components/sms.php
@@ -18,11 +18,11 @@
 
     <div class="row g-0">
        <div class="col-md-6">
-    <p style="font-size:15px;border-left: 3px solid #005992;padding:10px ">
+    <p style="font-size:13px;border-left: 3px solid #005992;padding:5px ">
           Envie e receba mensagens de texto de clientes do mundo todo de forma programática.     
     </p>
     
-    <p style="font-size:15px;border-left: 3px solid #005992;padding:10px ">
+    <p style="font-size:13px;border-left: 3px solid #005992;padding:5px ">
           Reduza as preocupações com conformidade — o mecanismo de conformidade 
           global da Vonage lida com as complexidades da comunicação mundial e adere 
           aos padrões de operadoras e redes em todo o mundo.
@@ -31,7 +31,7 @@
 
        <div class="col-md-6">
 
-  <p style="font-size:15px;border-left: 3px solid #005992;padding:10px ">
+  <p style="font-size:13px;border-left: 3px solid #005992;padding:5px ">
           Emparelhe com a API Verify para adicionar uma camada extra de segurança:
              valide usuários em seus dispositivos móveis usando autenticação de
               dois fatores.
