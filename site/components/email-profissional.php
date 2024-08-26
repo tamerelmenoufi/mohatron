00a1a80aaa4fc8a9d1f3435abd621df545d514a2
Raphassx <rapha.s_ouza@hotmail.com>
2024-08-26 10:07:13 -0400

    atualizar

--- site/components/email-profissional.php
+++ site/components/email-profissional.php
@@ -1,150 +1,110 @@
 <style>
-
-.wrap {
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.botao-sms {    
-  min-width: 300px;
-    min-height: 54px;
-    display: inline-flex;
-    font-family: 'Nunito', sans-serif;
-    font-size: 15px;
-    align-items: center;
-    justify-content: center;
-    text-transform: uppercase;
-    text-align: center;
-    letter-spacing: 1.3px;
-    font-weight: 700;
-    color: #fcfcff;
-    background: #4FD1C5;
-    background: linear-gradient(90deg, rgb(0 89 146) 0%, rgba(79, 209, 197, 1) 100%);
-    border: none;
-    border-radius: 1000px;
-    box-shadow: 12px 12px 24px rgba(79, 209, 197, .64);
-    transition: all 0.3s ease-in-out 0s;
+.bt-azul{
+    
+    display: inline-block;
     cursor: pointer;
-    outline: none;
     position: relative;
-    padding: 10px;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #144397;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
 }
-  
 
-.botao-sms::before {
-content: '';
-  border-radius: 1000px;
-  min-width: calc(300px + 12px);
-  min-height: calc(60px + 12px);
-  border: 6px solid #00FFCB;
-  box-shadow: 0 0 60px rgba(0,255,203,.64);
+.bt-azul:hover{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #32589b;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+    -webkit-animation: pulse 500ms;
+    animation: pulse 500ms;
+
+}
+.button {
+    border-radius: 4px;
+    background-color: #f9fafb;
+    border: none;
+    color: #0668bd;
+    text-align: center;
+    font-size: 14px;
+    padding: 9px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+    border-radius: 25px;
+    border-bottom: 4px solid #045ba8bf;
+    border-top: 4px solid #045ba8bf;
+  
+}
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   opacity: 0;
-  transition: all .3s ease-in-out 0s;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
 }
 
-.botao-sms:hover, 
-.botao-sms:focus {
-  color: #fff;
-  transform: translateY(-6px);
+.button:hover span {
+  padding-right: 25px;
 }
 
-.botao-sms:hover::before, 
-.botao-sms:focus::before {
+.button:hover span:after {
   opacity: 1;
+  right: 0;
 }
 
-.botao-sms::after {
-  content: '';
-  width: 30px; height: 30px;
-  border-radius: 100%;
-  border: 6px solid #00FFCB;
-  position: absolute;
-  z-index: -1;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  animation: ring 1.5s infinite;
-}
+ 
 
-.botao-sms:hover::after, 
-.botao-sms:focus::after {
-  animation: none;
-  display: none;
-}
 
-@keyframes ring {
-  0% {
-    width: 30px;
-    height: 30px;
-    opacity: 1;
-  }
-  100% {
-    width: 300px;
-    height: 300px;
-    opacity: 0;
-  }
-}
 </style>
-  <div class="row g-0" style="background:#005992">
+
+<div class="container aos-init aos-animate" style="margin-top:120px">
+<div class="row">
+    <div class="col-sm-5 col-md-6">
+
+    <p class="lead" style="text-align: left;">
+        Somos especializados em desenvolvimento de Sites Profissionais, Lojas Virtuais e 
+        Marketplace para Microempreendedores Individuais, Microempresas, Empresas de Pequeno,
+         Médio e Grande Porte.</p>
+
+    <p class="o-7" style="text-align: justify;">
+    No mercado desde 2009, a Mohatron Soluções em TI desenvolve ferramentas digitais modernas,
+     dinâmicas e de alta tecnologia, sempre com foco em resultados.
+      A experiência e expertise dos nossos profissionais é o grande diferencial nos 
+      trabalhos que executamos, pois em um mercado competitivo, para a sua empresa 
+      fazer bons negócios, é necessário que a estrutura online seja profissional.</p>
 
     
+    <button class="button"><span>Fale com especialista</span></button>
+  </div>
 
-
-
-    
-    <div class="col-md-6" style="padding:15px">
-
-       
-    <p style="font-size:30px;color:#fff;font-weight:bold">
-      Crie experiências de SMS que permitam que você alcance os usuários finais como e quando quiser
-    </p>
-
-
-      <div class="row g-0">
-       
-        <div class="col-md-6">
-    <p style="font-size:13px;border-left: 3px solid #fff;padding:5px ">
-          Envie e receba mensagens de texto de clientes do mundo todo de forma programática.     
-    </p>
-    
-    <p style="font-size:13px;border-left: 3px solid #fff;padding:5px ">
-          Reduza as preocupações com conformidade — o mecanismo de conformidade 
-          global da Vonage lida com as complexidades da comunicação mundial e adere 
-          aos padrões de operadoras e redes em todo o mundo.
-    </p>
-      </div>
-
-       <div class="col-md-6">
-
-    <p style="font-size:13px;border-left: 3px solid #fff;padding:5px ">
-          Emparelhe com a API Verify para adicionar uma camada extra de segurança:
-             valide usuários em seus dispositivos móveis usando autenticação de
-              dois fatores.
-    </p>
-      </div>
-          
-      <div class="wrap">
-  <button class="botao-sms">Saiba mais</button>
+<div class="col-sm-5 col-md-6">
+<img src="assets/img/img-apre1.jpg" class="img-responsive" style="border-radius:40px;width:100%;padding:10px">
 </div>
+   
 
 
-    </div>
-
-    <div class="col-md-6">
-
-          <div>
-             <img src="assets/img/img-sms.png" class="img-fluid " style="">
-         </div>
-    </div>
-<!-- fim imagem sms --->
-  
-  
-  
-  
-  </div><!--fim de tudo--->
+        </div>
+            </div>
+                </div>
+               
+             
