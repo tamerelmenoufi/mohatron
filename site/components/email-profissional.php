c57435e36608e1f8de5aecb2f05dca101c98504f
Raphassx <rapha.s_ouza@hotmail.com>
2024-08-26 15:47:32 -0400

    atualizar

--- site/components/email-profissional.php
+++ site/components/email-profissional.php
@@ -73,12 +73,111 @@
 }
 
  
+.recent-blog-posts .post-box .post-title {
+    font-size: 24px;
+    color: var(--color-secondary);
+    font-weight: 700;
+    margin: 15px 0 0 0;
+    position: relative;
+    transition: 0.3s;
+}
+
+.recent-blog-posts .post-box .post-title:hover {
+    font-size: 24px;
+    color: #fff;
+    font-weight: 700;
+    margin: 15px 0 0 0;
+    position: relative;
+    transition: 0.3s;
+}
+.botaobranco {
+    padding: 10px;
+    padding-left: 35px;
+    padding-right: 35px;
+    border-radius: 0;
+    font-size: 17px;
+    color: #144397;
+    background-color: #ffffff;
+    border-color: #ffffff;
+    margin: 10px;
+}
+.botaobranco:hover {
+    color: #fff;
+    background-color: #1f62d9;
+    border-color: #2468e1;
+}
+    .botaoroxo:hover {
+    color: #fff;
+    background-color: #574ec2;
+    border-color: #574ec2;
+}
+ .botaoroxo {
+    color: #fff;
+    background-color: #393287;
+    border-color: #393287;
+}
+.botao {
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 0.25rem;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, 
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  }
+
+  .recent-blog-posts .post-box .post-img {
+    overflow: hidden;
+    position: relative;
+    border-radius: 0px;
+}
+
+.gradiente-inicio{
+  
+  background-size: 400% 400%;
+  background-image: linear-gradient(-45deg, #144397, #0d87f0, #004786, #004585);
+  animation: mygradient 8s ease infinite;
+}
+
+
+.gradiente{
+  
+}
+
+@keyframes mygradient {
+  0% {
+      background-position: 0% 50%;
+  }
+  50% {
+      background-position: 100% 50%;
+  }
+  100% {
+      background-position: 0% 50%;
+  }
+}
+.media-body, .media-left, .media-right {
+    display: table-cell;
+    vertical-align: top;
+}
+
+
 
 
 </style>
 
-<div class=" aos-init aos-animate" style="background:#005992">
-<div class="row g-0" style="padding:25px">
+
+<div class="gradiente-inicio aos-init aos-animate" style="padding:30px" >
+       <div class="row g-0 gradiente" style="padding:25px">
     <div class="col-sm-5 col-md-12">
 
     <p class="lead" style="text-align: center;font-size:30px;color:#fff;font-weight:bold;margin-bottom:0px">
