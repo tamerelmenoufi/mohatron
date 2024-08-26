aa6482c92af9ef7135ac8ec1a5b24e246235b27a
Raphassx <rapha.s_ouza@hotmail.com>
2024-08-26 15:37:18 -0400

    atualizar

--- site/components/email-profissional.php
+++ site/components/email-profissional.php
@@ -97,6 +97,11 @@
     Com a nossa hospedagem de email, você se conecta ao seu servidor de e-mail 
     em dispositivos iOS ou Android, ou em clientes de e-mail como Webmail e Outlook.
     </p>
+<p class="o-7" style="text-align: center;font-size:16px;padding:5px;color:#fff ">
+Diferente de serviços de e-mail pessoal, os seus dados estão seguros com a gente. Conte com as melhores medidas de segurança do mercado para proteger o seu e-mail de acessos não autorizados. 
+Nossos servidores têm proteção avançada para prevenir ataques de spam.
+    </p>
+    
    
 </div>
 
