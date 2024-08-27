9ef3d90c5265802186b0163c31fd1071b36abf0b
livia128924 <livia128924@gmail.com>
2024-08-27 16:20:02 -0300

    Atualizações

--- site/components/email-profissional.php
+++ site/components/email-profissional.php
@@ -210,7 +210,7 @@ Nossos servidores têm proteção avançada para prevenir ataques de spam.
 
 
     <div style="text-align:right">
-        <img src="assets/img/e-mail-1.gif" class="img-fluid  " style="border-radius:25px;height:400px">
+        <img src="assets/img/e-mail-1.gif" class="img-fluid  " style="border-radius:25px;height:300px">
          </div>
 
     </div>
