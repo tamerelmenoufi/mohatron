465113c03323760ad1a33a5c4d715f855442f035
livia128924 <livia128924@gmail.com>
2024-08-28 09:37:30 -0300

    Atualizações

---
+++ site/components/hospedagem.php
@@ -0,0 +1,219 @@
+<style>
+.bt-azul{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #144397;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+}
+
+.bt-azul:hover{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #32589b;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+    -webkit-animation: pulse 500ms;
+    animation: pulse 500ms;
+
+}
+.button {
+    border-radius: 4px;
+    background-color: #f9fafb;
+    border: none;
+    color: #0668bd;
+    text-align: center;
+    font-size: 14px;
+    padding: 9px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+    border-radius: 25px;
+    border-bottom: 4px solid #045ba8bf;
+    border-top: 4px solid #045ba8bf;
+  
+}
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
+ 
+.recent-blog-posts .post-box .post-title {
+    font-size: 24px;
+    color: var(--color-secondary);
+    font-weight: 700;
+    margin: 15px 0 0 0;
+    position: relative;
+    transition: 0.3s;
+}
+
+.recent-blog-posts .post-box .post-title:hover {
+    font-size: 24px;
+    color: #fff;
+    font-weight: 700;
+    margin: 15px 0 0 0;
+    position: relative;
+    transition: 0.3s;
+}
+.botaobranco {
+    padding: 10px;
+    padding-left: 35px;
+    padding-right: 35px;
+    border-radius: 0;
+    font-size: 17px;
+    color: #144397;
+    background-color: #ffffff;
+    border-color: #ffffff;
+    margin: 10px;
+}
+.botaobranco:hover {
+    color: #fff;
+    background-color: #1f62d9;
+    border-color: #2468e1;
+}
+    .botaoroxo:hover {
+    color: #fff;
+    background-color: #574ec2;
+    border-color: #574ec2;
+}
+ .botaoroxo {
+    color: #fff;
+    background-color: #393287;
+    border-color: #393287;
+}
+.botao {
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 0.25rem;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, 
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  }
+
+  .recent-blog-posts .post-box .post-img {
+    overflow: hidden;
+    position: relative;
+    border-radius: 0px;
+}
+
+.gradiente-inicio{
+  
+  background-size: 400% 400%;
+  background-image: linear-gradient(-45deg, #144397, #0d87f0, #004786, #004585);
+  animation: mygradient 8s ease infinite;
+}
+
+
+.gradiente{
+  
+}
+
+@keyframes mygradient {
+  0% {
+      background-position: 0% 50%;
+  }
+  50% {
+      background-position: 100% 50%;
+  }
+  100% {
+      background-position: 0% 50%;
+  }
+}
+.media-body, .media-left, .media-right {
+    display: table-cell;
+    vertical-align: top;
+}
+
+.center {
+  width: 180px;
+  height: 60px;
+  position: absolute;
+}
+
+.btn {
+  width: 180px;
+  height: 60px;
+  cursor: pointer;
+  background: transparent;
+  border: 1px solid #91C9FF;
+  outline: none;
+  transition: 1s ease-in-out;
+}
+
+svg {
+  position: absolute;
+  left: 0;
+  top: 0;
+  fill: none;
+  stroke: #fff;
+  stroke-dasharray: 150 480;
+  stroke-dashoffset: 150;
+  transition: 1s ease-in-out;
+}
+
+.btn:hover {
+  transition: 1s ease-in-out;
+  background: #0f7156;
+}
+
+.btn:hover svg {
+  stroke-dashoffset: -480;
+}
+
+.btn span {
+  color: white;
+  font-size: 18px;
+  font-weight: 100;
+}
+
+
+</style>
+
+
+<section id="funcionalidades" class="product-features-list dark-white" data-gtm-vis-recent-on-screen103454_1499="26646" data-gtm-vis-first-on-screen103454_1499="26646" data-gtm-vis-total-visible-time103454_1499="100" data-gtm-vis-has-fired103454_1499="1"><div class="inner"><hgroup class="section-title"><h2>Hospedagem segura e de alta performance com funcionalidades essenciais</h2><p>A Locaweb cuida de tudo com recursos completos e hospedagem gerenciada</p></hgroup></div><div class="extendable-list 3"><ul class="line-bottom list"><li class="item round-icon"><span class="icon-restore"></span><h4 class="title">Backup diário</h4><p>Proteção garantida com restore dos últimos 7 dias</p></li><li class="item round-icon small"><span class="icon-wordpress"></span><h4 class="title">Recursos inovadores</h4><p>Integração com Google Analytics, PageSpeed e WordPress</p></li><li class="item round-icon small"><span class="icon-rocket"></span><h4 class="title">Excelente desempenho</h4><p>Seu site sempre online e otimizado, com 99,5% de uptime garantido</p></li><li class="item round-icon small"><span class="icon-mysql"></span><h4 class="title">MySQL e PostgreSQL</h4><p>Os principais bancos de dados de sistemas online</p></li></ul></div></section>
