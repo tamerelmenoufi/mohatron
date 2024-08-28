8779530f5f60a0ea7659d136763921dfe1450ee0
livia128924 <livia128924@gmail.com>
2024-08-28 15:19:07 -0300

    Atualizações

--- site/components/hospedagem.php
+++ site/components/hospedagem.php
@@ -240,7 +240,7 @@ section.beneficios .quadros .quadro .icone img {
 
       <div class="quadro col-md-4">
         <div class="icone um">
-          <img src="./assets/files/images/icone-escala.svg">
+        <i class="fa-solid fa-chart-line"   style="font-size:22px;color:#fff;border: solid 3px #fff;border-radius:25px;padding:8px;background: #0d77ad;" > </i>
         </div>
         <div class="texto">
           <h3>Escala</h3>
