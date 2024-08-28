3fec31af852167a7c3215ada23050d4af68534b2
livia128924 <livia128924@gmail.com>
2024-08-28 13:20:35 -0300

    Atualizações

--- site/components/hospedagem.php
+++ site/components/hospedagem.php
@@ -209,9 +209,7 @@ section.beneficios .quadros .quadro .icone img {
 
       <div class="quadro col-md-4">
         <div class="icone um">
-          <img src="assets/img/sites.png">
-          <div><i class="fa-solid fa-bolt" style="font-size:22px;color:#fff"></i></div>
-          
+          <div><i class="fa-solid fa-bolt" style="font-size:22px;color:#fff"></i></div>         
         </div>
         <div class="texto">
           <h3>LiteSpeed</h3>
