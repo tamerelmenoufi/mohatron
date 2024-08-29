6e8c0bc0c76e5f148b5b834c36b138ecfedc504b
livia128924 <livia128924@gmail.com>
2024-08-29 10:19:44 -0300

    Atualizações

--- site/components/hospedagem.php
+++ site/components/hospedagem.php
@@ -1,103 +1,94 @@
   <!-- ======= Recent Blog Posts Section ======= -->
 
     <style>
-
-.recent-blog-posts .post-box .post-title {
-    font-size: 24px;
-    color: var(--color-secondary);
-    font-weight: 700;
-    margin: 15px 0 0 0;
-    position: relative;
-    transition: 0.3s;
+.wrap {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.recent-blog-posts .post-box .post-title:hover {
-    font-size: 24px;
-    color: #fff;
-    font-weight: 700;
-    margin: 15px 0 0 0;
-    position: relative;
-    transition: 0.3s;
-}
-.botaobranco {
-    padding: 10px;
-    padding-left: 35px;
-    padding-right: 35px;
-    border-radius: 0;
-    font-size: 17px;
-    color: #144397;
-    background-color: #ffffff;
-    border-color: #ffffff;
-    margin: 10px;
-}
-.botaobranco:hover {
-    color: #fff;
-    background-color: #1f62d9;
-    border-color: #2468e1;
-}
-    .botaoroxo:hover {
-    color: #fff;
-    background-color: #574ec2;
-    border-color: #574ec2;
-}
- .botaoroxo {
-    color: #fff;
-    background-color: #393287;
-    border-color: #393287;
-}
-.botao {
-    display: inline-block;
-    font-weight: 400;
+.botao-sms {    
+  min-width: 300px;
+    min-height: 54px;
+    display: inline-flex;
+    font-family: 'Nunito', sans-serif;
+    font-size: 15px;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
     text-align: center;
-    white-space: nowrap;
-    vertical-align: middle;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    border: 1px solid transparent;
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    border-radius: 0.25rem;
-    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, 
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-
-  }
-
-  .recent-blog-posts .post-box .post-img {
-    overflow: hidden;
+    letter-spacing: 1.3px;
+    font-weight: 700;
+    color: #fcfcff;
+    background: #4FD1C5;
+    background: linear-gradient(90deg, rgb(0 89 146) 0%, rgba(79, 209, 197, 1) 100%);
+    border: none;
+    border-radius: 1000px;
+    box-shadow: 12px 12px 24px rgba(79, 209, 197, .64);
+    transition: all 0.3s ease-in-out 0s;
+    cursor: pointer;
+    outline: none;
     position: relative;
-    border-radius: 0px;
+    padding: 10px;
 }
-
-.gradiente-inicio{
   
-  background-size: 400% 400%;
-  background-image: linear-gradient(-45deg, #144397, #0d87f0, #004786, #004585);
-  animation: mygradient 8s ease infinite;
+
+.botao-sms::before {
+content: '';
+  border-radius: 1000px;
+  min-width: calc(300px + 12px);
+  min-height: calc(60px + 12px);
+  border: 6px solid #00FFCB;
+  box-shadow: 0 0 60px rgba(0,255,203,.64);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 0;
+  transition: all .3s ease-in-out 0s;
 }
 
-
-.gradiente{
-  
-}
-.media-body, .media-left, .media-right {
-    display: table-cell;
-    vertical-align: top;
+.botao-sms:hover, 
+.botao-sms:focus {
+  color: #fff;
+  transform: translateY(-6px);
 }
 
+.botao-sms:hover::before, 
+.botao-sms:focus::before {
+  opacity: 1;
+}
 
+.botao-sms::after {
+  content: '';
+  width: 30px; height: 30px;
+  border-radius: 100%;
+  border: 6px solid #00FFCB;
+  position: absolute;
+  z-index: -1;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  animation: ring 1.5s infinite;
+}
 
-@keyframes mygradient {
+.botao-sms:hover::after, 
+.botao-sms:focus::after {
+  animation: none;
+  display: none;
+}
+
+@keyframes ring {
   0% {
-      background-position: 0% 50%;
-  }
-  50% {
-      background-position: 100% 50%;
+    width: 30px;
+    height: 30px;
+    opacity: 1;
   }
   100% {
-      background-position: 0% 50%;
+    width: 300px;
+    height: 300px;
+    opacity: 0;
   }
 }
 .media-body, .media-left, .media-right {
@@ -270,7 +261,10 @@ section.beneficios .quadros .quadro .icone img {
         </div>
       </div>
       </div>
-      <div class="inner"><a data-ga-trigger="" data-ga-category="" data-ga-action="" data-ga-label="" href="#planos" class="button-primary">Quero contratar</a></div>
+      <div class="wrap">
+  <button class="botao-sms">Contrate agora</button>
+</div>
+
 
     </div>
   </section>
