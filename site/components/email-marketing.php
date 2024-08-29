e148fe7ec341111815abd918f6c9bb64d40054ab
livia128924 <livia128924@gmail.com>
2024-08-29 15:30:44 -0300

    Atualizações

--- site/components/email-marketing.php
+++ site/components/email-marketing.php
@@ -234,7 +234,7 @@ Crie fantásticas campanhas de marketing por correio eletrónico do início ao f
  Você pode alcançar uma grande quantidade de pessoas de forma direta e imediata, com uma lista de contatos
  bem segmentada.
 
- <p><i style="color:#fff"class="fa-solid fa-send-backward"></i> Modelos de correio eletrónico</p>
+ <b><i style="color:#fff"class="fa-solid fa-send-backward"></i> Modelos de correio eletrónico</b>
  
 
 
