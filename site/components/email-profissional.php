7756ddedf2ea268f317e403489b400f4fac46cbb
livia128924 <livia128924@gmail.com>
2024-08-29 10:48:36 -0300

    Atualizações

--- site/components/email-profissional.php
+++ site/components/email-profissional.php
@@ -232,7 +232,7 @@ svg {
 }
 
 </style>
-<div class="fundo-verde d-none d-sm-block" style="height:120px"></div> 
+<div class="fundo-verde2 d-none d-sm-block" style="height:120px"></div> 
 <div class=" aos-init aos-animate" style="padding:30px;background:#006b62;padding-bottom:0px" >
 <div class="  " style="" >
        <div class="row g-0 " style="padding:25px">
