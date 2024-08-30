460ef13e96f90247c0608feca51e16ca001508c3
livia128924 <livia128924@gmail.com>
2024-08-30 11:58:39 -0300

    Atualizações

--- site/components/fale-conosco.php
+++ site/components/fale-conosco.php
@@ -105,18 +105,18 @@
 
 
 <form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
-    <label for="validationCustom01" class="form-label">First name</label>
+  <div class="col-md-12">
+    <label for="validationCustom01" class="form-label">Nome Completo</label>
     <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
     <div class="valid-feedback">
-      Looks good!
+      Correto
     </div>
   </div>
-  <div class="col-md-4">
-    <label for="validationCustom02" class="form-label">Last name</label>
+  <div class="col-md-12">
+    <label for="validationCustom02" class="form-label">E-mail</label>
     <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
     <div class="valid-feedback">
-      Looks good!
+      Correto
     </div>
   </div>
   <div class="col-md-4">
