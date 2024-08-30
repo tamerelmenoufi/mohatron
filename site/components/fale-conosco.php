b70ae1582582a38afadbfef5d3e99afbff761a4a
livia128924 <livia128924@gmail.com>
2024-08-30 10:14:30 -0300

    Atualizações

---
+++ site/components/fale-conosco.php
@@ -0,0 +1,111 @@
+<style>
+.bt-azul{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #144397;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+}
+
+.bt-azul:hover{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #32589b;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+    -webkit-animation: pulse 500ms;
+    animation: pulse 500ms;
+
+}
+.button {
+    border-radius: 4px;
+    background-color: #f9fafb;
+    border: none;
+    color: #0668bd;
+    text-align: center;
+    font-size: 14px;
+    padding: 9px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+    border-radius: 25px;
+    border-bottom: 4px solid #045ba8bf;
+    border-top: 4px solid #045ba8bf;
+  
+}
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
+ 
+
+
+</style>
+
+<section  id="apresentacao" class="" style="padding:0px">
+<div class="container aos-init aos-animate" style="margin-top:120px">
+<div class="row">
+    <div class="col-sm-5 col-md-6">
+
+    <p class="lead" style="text-align: left;">
+        Somos especializados em desenvolvimento de Sites Profissionais, Lojas Virtuais e 
+        Marketplace para Microempreendedores Individuais, Microempresas, Empresas de Pequeno,
+         Médio e Grande Porte.</p>
+
+    <p class="o-7" style="text-align: justify;">
+    No mercado desde 2009, a Mohatron Soluções em TI desenvolve ferramentas digitais modernas,
+     dinâmicas e de alta tecnologia, sempre com foco em resultados.
+      A experiência e expertise dos nossos profissionais é o grande diferencial nos 
+      trabalhos que executamos, pois em um mercado competitivo, para a sua empresa 
+      fazer bons negócios, é necessário que a estrutura online seja profissional.</p>
+
+    
+    <button class="button"><span>Fale com especialista</span></button>
+  </div>
+
+<div class="col-sm-5 col-md-6">
+<img src="assets/img/img-apre1.jpg" class="img-responsive" style="border-radius:40px;width:100%;padding:10px">
+</div>
+   
+
+
+        </div>
+            </div>
+                </div>
+               
+</section>
