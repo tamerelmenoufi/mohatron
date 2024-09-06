096d738d293a2d720bfd932d0afeca48426c845a
livia128924 <livia128924@gmail.com>
2024-09-06 13:18:17 -0300

    Atualizações

---
+++ site/components/email-profissional1.php
@@ -0,0 +1,318 @@
+<style>
+.bt-azul{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #144397;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+}
+
+.bt-azul:hover{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #32589b;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+    -webkit-animation: pulse 500ms;
+    animation: pulse 500ms;
+
+}
+.button {
+    border-radius: 4px;
+    background-color: #f9fafb;
+    border: none;
+    color: #0668bd;
+    text-align: center;
+    font-size: 14px;
+    padding: 9px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+    border-radius: 25px;
+    border-bottom: 4px solid #045ba8bf;
+    border-top: 4px solid #045ba8bf;
+  
+}
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
+ 
+.recent-blog-posts .post-box .post-title {
+    font-size: 24px;
+    color: var(--color-secondary);
+    font-weight: 700;
+    margin: 15px 0 0 0;
+    position: relative;
+    transition: 0.3s;
+}
+
+.recent-blog-posts .post-box .post-title:hover {
+    font-size: 24px;
+    color: #fff;
+    font-weight: 700;
+    margin: 15px 0 0 0;
+    position: relative;
+    transition: 0.3s;
+}
+.botaobranco {
+    padding: 10px;
+    padding-left: 35px;
+    padding-right: 35px;
+    border-radius: 0;
+    font-size: 17px;
+    color: #144397;
+    background-color: #ffffff;
+    border-color: #ffffff;
+    margin: 10px;
+}
+.botaobranco:hover {
+    color: #fff;
+    background-color: #1f62d9;
+    border-color: #2468e1;
+}
+    .botaoroxo:hover {
+    color: #fff;
+    background-color: #574ec2;
+    border-color: #574ec2;
+}
+ .botaoroxo {
+    color: #fff;
+    background-color: #393287;
+    border-color: #393287;
+}
+.botao {
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 0.25rem;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, 
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+
+  }
+
+  .recent-blog-posts .post-box .post-img {
+    overflow: hidden;
+    position: relative;
+    border-radius: 0px;
+}
+
+.gradiente-inicio{
+  
+  background-size: 400% 400%;
+  background-image: linear-gradient(-45deg, #144397, #0d87f0, #004786, #004585);
+  animation: mygradient 8s ease infinite;
+}
+
+
+.gradiente{
+  
+}
+
+@keyframes mygradient {
+  0% {
+      background-position: 0% 50%;
+  }
+  50% {
+      background-position: 100% 50%;
+  }
+  100% {
+      background-position: 0% 50%;
+  }
+}
+.media-body, .media-left, .media-right {
+    display: table-cell;
+    vertical-align: top;
+}
+
+.center {
+  width: 180px;
+  height: 60px;
+  position: absolute;
+}
+
+.btn {
+  width: 180px;
+  height: 60px;
+  cursor: pointer;
+  background: transparent;
+  border: 1px solid #91C9FF;
+  outline: none;
+  transition: 1s ease-in-out;
+}
+
+svg {
+  position: absolute;
+  left: 0;
+  top: 0;
+  fill: none;
+  stroke: #fff;
+  stroke-dasharray: 150 480;
+  stroke-dashoffset: 150;
+  transition: 1s ease-in-out;
+}
+
+.btn:hover {
+  transition: 1s ease-in-out;
+  background: #0f7156;
+}
+
+.btn:hover svg {
+  stroke-dashoffset: -480;
+}
+
+.btn span {
+  color: white;
+  font-size: 18px;
+  font-weight: 100;
+}
+
+.fundo-verde{
+ background-image:url(assets/img/fundo-verde.png);
+ background-repeat: no-repeat center fixed;
+    background-size: cover;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;sx
+}
+.fundo-verde2{
+ background-image:url(assets/img/fundo-verde2.png);
+ background-repeat: no-repeat center fixed;
+    background-size: cover;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;sx
+}
+
+</style>
+
+<section  id="email-profissional" class="" style="padding:0px">
+  <div class="fundo-verde2 d-none d-sm-block" style="height:112px"></div> 
+    <div class=" aos-init aos-animate" style="padding:30px;background:#006b62;padding-bottom:0px" >
+       <div class="row g-0 " style="padding:25px">
+          
+       <div class="col-sm-5 col-md-9" style="">
+    <p class="lead" style="font-size:35px;color:#fff;font-weight:bold;margin-bottom:0px">
+    Crie um <b style="text-align: center;font-size:30px;color:#0ed9c8;">E-mail Profissional</b><br>
+  com o nome da sua marca!
+  </p>
+    <p class="o-7" style="margin-top:5px; font-size:19px;padding:5px;color:#fff ">
+   Planos a partir de <b style="color:#0ed9c8;font-size:25px">R$ 9,90 </b> por mês.
+   </p>
+
+   <div style="margin-bottom:15px">
+      <button class="btn">
+        <a titulo="E-mail Profissional"  class="teste" title="segmentos" href="#mostrar_planos">
+        <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+        </svg>
+        <span style="font-size:14px">Conheça nossos planos</span>
+        </a>
+      </button>
+    </div>
+
+<div class="row g-0 " style="">
+      <div class=" col-md-4" style="padding:5px">
+        <div style="background: #ffffff;border-radius: 10px;
+        box-shadow: 2px 1px 3px #0ed9c8;padding: 15px 15px;">
+        <i style="font-size:20px" class="fa-solid fa-envelope-open-text"></i><br><br>
+         <p style="color: #615d5d;font-size:14px;margin-bottom:2px">
+         E-mail personalizado com o seu <br> domínio <b>@meunegocio.com.br</b>
+        </p>
+        </div>
+      </div>
+
+      <div class=" col-md-4" style="padding:5px">
+        <div style="background: #ffffff;border-radius: 10px;
+        box-shadow: 2px 1px 3px #0ed9c8;padding: 15px 15px;">
+        <i style="font-size:20px" class="fa-solid fa-cloud-arrow-up"></i><br><br>
+         <p style="color: #615d5d;font-size:14px;margin-bottom:2px">
+         Planos com até<b> 50 GB</b> de <br> armazenamento de e-mail.
+        </p>
+        </div>
+      </div>
+
+      <div class=" col-md-4" style="padding:5px">
+        <div style="background: #ffffff;border-radius: 10px;
+        box-shadow: 2px 1px 3px #0ed9c8;padding: 15px 15px;">
+        <i style="font-size:20px" class="fa-solid fa-lock"></i><br><br>
+         <p style="color: #615d5d;font-size:14px;margin-bottom:2px">
+         E-mail totalmente <b>seguro</b> e <b>sem <br> anúncios.</b>
+        </p>
+        </div>
+      </div>
+
+
+     
+
+
+</div>
+
+    
+    </div>
+
+
+        <div class="col-sm-5 col-md-3">
+
+
+
+<div style="text-align:right">
+    <img src="assets/img/e-mail-1.gif" class="img-fluid  " style="border-radius:25px;height:275px">
+     </div>
+
+</div>
+
+
+
+                </div>
+            </div>   
+      <div  class="fundo-verde d-none d-sm-block" style="height:115px"></div>  
+</section>
