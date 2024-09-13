76aa996018ecc352dd77625f6e8df1c27e14eaf4
livia128924 <livia128924@gmail.com>
2024-09-13 17:33:10 -0300

    Atualizações

--- site/assets/lib/scripts.php
+++ site/assets/lib/scripts.php
@@ -34,3 +34,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         ttq.page();
     }(window, document, 'ttq');
 </script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11464358068">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11464358068');
+</script>
