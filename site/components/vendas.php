8931a21f6fd178a435b37712e937767fe1efa70f
Raphassx <rapha.s_ouza@hotmail.com>
2024-09-23 16:12:20 -0300

    atualizar

---
+++ site/components/vendas.php
@@ -0,0 +1,100 @@
+<style>
+.bt-azul{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #144397;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+}
+
+.bt-azul:hover{
+    
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    outline: none !important;
+    border: none;
+    background: #32589b;
+    color: #fff !important;
+    -webkit-border-radius: 30px;
+    -moz-border-radius: 30px;
+    border-radius: 30px;
+    -webkit-animation: pulse 500ms;
+    animation: pulse 500ms;
+
+}
+.button {
+    border-radius: 4px;
+    background-color: #f9fafb;
+    border: none;
+    color: #0668bd;
+    text-align: center;
+    font-size: 14px;
+    padding: 9px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+    border-radius: 25px;
+    border-bottom: 4px solid #045ba8bf;
+    border-top: 4px solid #045ba8bf;
+  
+}
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
+ 
+
+
+</style>
+
+<section  id="vendas" class="" style="padding:0px">
+<div class="container aos-init aos-animate" style="margin-top:120px">
+<div class="row">
+
+				
+        <div class=" col-md-12">
+  
+    <img class="img-fluid" src="assets/img/quem-s.jpeg" style="padding: 7px;border-radius:25px">
+      <p style="font-size: 15px;color: #005992;font-family:Times New Roman,Times,serif;font-size:18px;border-left: #0076aa solid 3px;padding: 10px;"> SOMOS MOVIDOS <br>
+    PAGINA EM CONSTRUÇÃO
+      </p>
+        </div>
+      
+  
+    
+
+
+            </div>
+                </div>
+               
+</section>
